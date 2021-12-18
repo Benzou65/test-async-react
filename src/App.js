fbf4bac9505d10e7d25d17e7715b0beaa236b523
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2021-12-18 01:09:06 +0100

    feat: add reducer and setState example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useReducer } from "react";
 
-function App() {
+function init(initialCount) {
+  return { count: initialCount };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return init(action.payload);
+    default:
+      throw new Error();
+  }
+}
+
+function App({ initialCount = 0 }) {
+  const [num, setNum] = useState(0);
+  const [numbers, setNumbers] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialCount, init);
+
+  const handleClick = () => {
+    setNum(num + 1);
+    setNum(num + 1);
+  };
+
+  const addToNumbers = () => {
+    setNumbers((prev) => prev + 4);
+    setNumbers((prev) => prev - 2);
+    setNumbers((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={handleClick}>Click</button>
+          <p>{num}</p>
+        </div>
+        <div>
+          <button onClick={addToNumbers}>Click</button>
+          <p>{numbers}</p>
+        </div>
+        <div>
+          <p>Count: {state.count}</p>
+          <button
+            onClick={() => dispatch({ type: "reset", payload: initialCount })}
+          >
+            Reset
+          </button>
+          <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+          <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        </div>
       </header>
     </div>
   );
